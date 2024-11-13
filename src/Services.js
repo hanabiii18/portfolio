c5b8b64f467500b5f970606ba0fd3e48d837691f
Miyukkii <crishaann.dandoy@gmail.com>
2024-11-13 14:19:32 +0800

    ks

--- src/Services.js
+++ src/Services.js
@@ -17,11 +17,11 @@ function Services() {
 
   return (
     <section className="section__container service__container" id="service">
-      <h2 className="section__header">My Best Services</h2>
+      <h2 className="section__header">My Skills</h2>
       <div className="service__grid">
         <div className="service__card">
           <span><img src="/icons/java.png" alt="Website Design Icon" /></span>
-          <h4>Website Design</h4>
+          <h4>Java</h4>
           <p>
             We craft user-friendly interfaces that engage visitors and help you
             achieve your online goals with minimum efforts.
@@ -29,7 +29,7 @@ function Services() {
         </div>
         <div className="service__card">
           <span><img src="/icons/html.png" alt="E-commerce Solutions Icon" /></span>
-          <h4>E-commerce Solutions</h4>
+          <h4>HTML</h4>
           <p>
             We build secure, scalable, and user-centric online stores that
             enhance the shopping experience and drive conversions.
@@ -37,7 +37,7 @@ function Services() {
         </div>
         <div className="service__card">
           <span><img src="/icons/php.png" alt="Mobile Development Icon" /></span>
-          <h4>Mobile Development</h4>
+          <h4>PHP</h4>
           <p>
             From iOS to Android, we create apps that deliver seamless
             performance and keep users coming back for more.
@@ -45,7 +45,7 @@ function Services() {
         </div>
         <div className="service__card">
           <span><img src="/icons/python.png" alt="Content Marketing Icon" /></span>
-          <h4>Content Marketing</h4>
+          <h4>Python</h4>
           <p>
             Our services include creating blog posts, videos, and social media
             content that drives traffic and engagement.
@@ -53,7 +53,7 @@ function Services() {
         </div>
         <div className="service__card">
           <span><img src="/icons/bootstrap.png" alt="SEO Icon" /></span>
-          <h4>SEO</h4>
+          <h4>Bootstrap</h4>
           <p>
             Our SEO strategies are tailored to your specific goals, helping you
             reach your target audience and grow your online presence.
@@ -61,7 +61,7 @@ function Services() {
         </div>
         <div className="service__card">
           <span><img src="/icons/mysql.png" alt="Digital Marketing Icon" /></span>
-          <h4>Digital Marketing</h4>
+          <h4>MySQL</h4>
           <p>
             From pay-per-click (PPC) advertising to social media marketing, we
             ensure your brand stands out in the crowded digital landscape.
